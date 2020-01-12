200ef45d98b8a503c23ecba267b5f459e04cd966
RGB Studios <justingolden21@gmail.com>
2020-01-11 17:03:33 -0800

    testing

--- scripts.js
+++ scripts.js
@@ -2,9 +2,33 @@
 
 $(function() {
 
+	// $('#we-are').hide().fadeIn(2000);
+
+	$('#top-logo').css('opacity', 0);
+	$('#slogan').css('opacity', 0);
+
+	$('#we-are').css('opacity', 0).animate({
+		opacity: 1,
+
+	}, 1500, ()=> {
+		$('#top-logo').animate({
+			opacity: 1,
+
+		}, 1500, ()=> {
+			$('#slogan').animate({
+				opacity: 1,
+
+			}, 1500);
+		});
+	});
+
+	new fullpage('#fullpage', {
+		sectionsColor: ['#ecd317', '#cab209', '#ad960e', '#ecd317', '#cab209'],
+		navigation: true,
+		navigationTooltips: ['Home', 'Mission', 'Events', 'Team', 'Contact'],
+	});
+
+
+
+
 });
-
-window.onkeyup = function(e) {
-	let key = e.keyCode ? e.keyCode : e.which;
-}
-
